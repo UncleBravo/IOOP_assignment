c452a7dc59922cc22f13accc39c7ce5883d3925e
UncleBravo <engelbertanthonypereira@gmail.com>
2024-03-31 15:41:56 +0800

    changes

--- IOOP_assignment/TrainingSchedule.cs
+++ IOOP_assignment/TrainingSchedule.cs
@@ -38,7 +38,7 @@ namespace IOOP_assignment
             SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"].ToString());
             con.Open();
 
-            SqlCommand cmdInsert = new SqlCommand("Insert into TrainingSchedule (refno, coachid, time, date) VALUES (@r, @cid, @time, @date)", con);
+            SqlCommand cmdInsert = new SqlCommand("Insert into TrainingSchedule (Schedule_Refno, Coach_RefNo, time, date) VALUES (@r, @cid, @time, @date)", con);
 
             cmdInsert.Parameters.AddWithValue("@r", reference_number);
             cmdInsert.Parameters.AddWithValue("@cid", coach_id);
@@ -63,13 +63,13 @@ namespace IOOP_assignment
             SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"].ToString());
             con.Open();
 
-            SqlCommand cmdCheck = new SqlCommand("SELECT COUNT(*) from TrainingSchedule where RefNo=@refno", con);
+            SqlCommand cmdCheck = new SqlCommand("SELECT COUNT(*) from TrainingSchedule where Schedule_Refno=@refno", con);
             cmdCheck.Parameters.AddWithValue("@refno", reference_number);
 
             int count = Convert.ToInt32(cmdCheck.ExecuteScalar());
             if (count > 0)
             {
-                SqlCommand cmdDelete = new SqlCommand("Delete FROM TrainingSchedule WHERE RefNo = @refno", con);
+                SqlCommand cmdDelete = new SqlCommand("Delete FROM TrainingSchedule WHERE Schedule_Refno = @refno", con);
                 cmdDelete.Parameters.AddWithValue("@refno", reference_number);
                 int rowaffected = cmdDelete.ExecuteNonQuery();
 
@@ -96,13 +96,13 @@ namespace IOOP_assignment
             SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"].ToString());
             con.Open();
 
-            SqlCommand cmdCheck = new SqlCommand("SELECT COUNT(*) from TrainingSchedule where RefNo=@refno", con);
+            SqlCommand cmdCheck = new SqlCommand("SELECT COUNT(*) from TrainingSchedule where Schedule_Refno=@refno", con);
             cmdCheck.Parameters.AddWithValue("@refno", reference_number);
 
             int count = Convert.ToInt32(cmdCheck.ExecuteScalar());
             if (count > 0)
             {
-                SqlCommand cmdUpdate = new SqlCommand("UPDATE TrainingSchedule SET RefNo = @refno, CoachID = @coachid, Time = @time, Date = @date WHERE Refno = @refno", con);
+                SqlCommand cmdUpdate = new SqlCommand("UPDATE TrainingSchedule SET Schedule_Refno = @refno, Coach_RefNo = @coachid, time = @time, date = @date WHERE Schedule_Refno = @refno", con);
                 cmdUpdate.Parameters.AddWithValue("@refno", reference_number);
                 cmdUpdate.Parameters.AddWithValue("@coachid", coach_id); 
                 cmdUpdate.Parameters.AddWithValue("@time", time); 
